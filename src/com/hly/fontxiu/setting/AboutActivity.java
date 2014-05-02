9960e0111b5cf04d91ab3948d986bc3020067fa3
kalus <741470894@qq.com>
2014-05-02 16:32:38 +0800

    版本号的修改

--- src/com/hly/fontxiu/setting/AboutActivity.java
+++ src/com/hly/fontxiu/setting/AboutActivity.java
@@ -1,9 +1,13 @@
 package com.hly.fontxiu.setting;
 
-import com.hly.fontxiu.R;
-
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.hly.fontxiu.R;
 
 public class AboutActivity extends Activity {
 
@@ -11,5 +15,16 @@ public class AboutActivity extends Activity {
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
 		setContentView(R.layout.about_layout);
+		TextView tv = (TextView) findViewById(R.id.txt_version);
+		
+		PackageManager pm = getPackageManager();//context为当前Activity上下文 
+		PackageInfo pi;
+		try {
+			pi = pm.getPackageInfo(getPackageName(), 0);
+			String versionName = pi.versionName;
+			tv.setText("版本号："+versionName);
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
