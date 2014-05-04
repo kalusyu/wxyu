11f5934c70acdfcbf5f32581d5d58f2213774534
kalus <741470894@qq.com>
2014-05-04 12:46:02 +0800

    文本规范化

--- Android.mk
+++ Android.mk
@@ -35,14 +35,27 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_STATIC_JAVA_LIBRARIES := activation additional mail youmi android-support-v4
+
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
+
 
 LOCAL_PACKAGE_NAME := FontXiuXiu
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
+
+##################################################
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := activation:libs/activation.jar \
+	additional:libs/additionnal.jar \
+	mail:libs/mail.jar \
+	youmi:libs/YoumiSdk_v4.08_2014-04-16.jar
+
+include $(BUILD_MULTI_PREBUILT)
 
 # additionally, build unit tests in a separate .apk
 include $(call all-makefiles-under,$(LOCAL_PATH))
